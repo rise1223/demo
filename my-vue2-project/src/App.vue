85ca8de25bbe19385bb776daff0342dd99ae937a
Rise <2810411246@qq.com>
2025-05-26 22:14:51 +0800

    feat: duyi

---
+++ my-vue2-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <MySwitch />
+  </div>
+</template>
+
+<script>
+import MySwitch from './components/MySwitch.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    MySwitch
+  }
+}
+</script>
+
+<style scoped></style>
